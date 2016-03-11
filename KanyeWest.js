fb05208a0339472a99029511c58810984522b1c8
Jamie Cruwys <me@jamiecruwys.co.uk>
2016-03-11 21:43:51 +0000

    Removed ' character

--- KanyeWest.js
+++ KanyeWest.js
@@ -6,7 +6,7 @@ var quotes =
     "So, the voices in my head told me go, and then I just walked up, like, halfway up the stage... But I just didn't really want to take away from Beck's moment, or the time he's having to talk. It was kind of a joke - like the Grammys themselves",
     "My greatest pain in life is that I will never be able to see myself perform live.",
     "I think what Kanye West is going to mean is something similar to what Steve Jobs means. I am undoubtedly, you know, Steve of internet, downtown, fashion, culture. Period. By a long jump. I honestly feel that because Steve has passed, you know.",
-    "'I hate when I'm on a flight and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle",
+    "I hate when I'm on a flight and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle",
     "One of the problems with being a bubbling source of creativity - it's like I'm bubbling in a laboratory, and if you don't put a cap on it, at one point it will, like, break the glass. If I can hone that... then I have, like, nuclear power, like a superhero, like Cyclops when he puts his glasses on.",
     "You should only believe about 90 percent of what I say. As a matter of fact, don't even believe anything that I'm saying at all. I could be completely fucking with you, and the world, the entire time.",
     "I don't even listen to rap. My apartment is too nice to listen to rap in.",
